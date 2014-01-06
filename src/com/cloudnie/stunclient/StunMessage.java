bccb550e6de8e9a853b657320d5dfae652e768c7
Tony Nie <cloudnie@gmail.com>
2014-01-06 22:42:00 +0800

    StunMessage: fix the issue of convertion of RAW to attr
    Change-Id: If290a96d5a4f47882e6955311134cd9cb2ee9acc
    Signed-off-by: Tony Nie <cloudnie@gmail.com>

--- src/com/cloudnie/stunclient/StunMessage.java
+++ src/com/cloudnie/stunclient/StunMessage.java
@@ -1,11 +1,11 @@
 package com.cloudnie.stunclient;
 import java.lang.System;
-import java.io.*;
-import java.net.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+
+import sun.security.util.Length;
 
 /*			Format of STUN Message
  *  0                   1                   2                   3
@@ -33,8 +33,7 @@ public class StunMessage {
 		mType = (value[0] << 8) | value[1];
 		mLength = (value[2] << 8) | value[3];
 		System.arraycopy(value, 4, mTransID, 0, 16);
-
-		rawToAttributes(value);
+		rawToAttributes(value, mLength);
 	}
 
 	public StunMessage(int type) {
@@ -107,20 +106,23 @@ public class StunMessage {
 		return mAttriNum;
 	}
 
-	private void rawToAttributes(byte[] value) {
+	private void rawToAttributes(byte[] value, int length) {
 		int pos = 20;
 
 		assert(value.length >= 20);
+		assert(value.length >= length);
 		mAttriNum = 0;
 
-		while(pos < value.length) {
-			int __len = (value[1] << 8) | value[2];
+		while(pos < length) {
+			int __len = (value[pos + 2] << 8) | value[pos + 3];
 
 			if (__len > (value.length - 20 - 4)) {
 				/*TODO: throw exception */
+			} else if (__len <= 0) {
+				break;
 			}
-
-			byte attri[] = new byte[__len];
+			
+			byte attri[] = new byte[__len + 4];
 			System.arraycopy(value, 20, attri, 0, __len + 4);
 			StunAttribute __attri = new StunAttribute(attri);
 			mAttris.add(__attri);
