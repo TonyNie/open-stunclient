ece684fc2c0e1b25d172874416bfe72e01ce0b38
Tony Nie <cloudnie@gmail.com>
2014-01-06 21:34:10 +0800

    StunClient: test UDP connectivity
    Change-Id: Icae8f06f2ddcc00b9ae67ba7479b875aba0893b0
    Signed-off-by: Tony Nie <cloudnie@gmail.com>

--- com/cloudnie/stunclient/StunClient.java
+++ com/cloudnie/stunclient/StunClient.java
@@ -5,8 +5,9 @@ import java.io.InterruptedIOException;
 import java.net.DatagramPacket;  
 import java.net.DatagramSocket;  
 import java.net.InetAddress; 
+import java.util.Arrays;
 
-class StunClient {
+public class StunClient {
 	private String mSTUNServerName = "stun.ekiga.net";
 	private short mSTUNServerPort = 3478;
 	private int mTimeout = 500;
@@ -43,7 +44,21 @@ class StunClient {
 		return response;
 	}
 
+	public int testUDPConnectivity() {
+		try {
+			sendBindingRequest();
+			StunMessage response = getResponse();
+			byte raw[] = response.getRaw();
+			System.out.println(Arrays.toString(raw));
+		} catch (IOException exception) {
+		} finally {
+		}
+
+		return 0;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("hello, world");
+		StunClient client = new StunClient();
+		client.testUDPConnectivity();
 	}
 };
